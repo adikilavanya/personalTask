7ce1a20d068a089cdeea93326ffde16264550654
adikilavanya <151808615+adikilavanya@users.noreply.github.com>
2024-09-19 14:37:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Personal Task Manger</h1>
+      <h2 className="h2">
+        Personal task management can be applied to a variety of activities,
+        including work projects and personal errands. There's no single "best
+        way" to do personal task management, and people may prefer different
+        methods depending on their preferences. Some people use mobile apps,
+        while others may prefer to write things down in a planner or use a
+        computer program.
+      </h2>
+    </div>
+  );
+}
